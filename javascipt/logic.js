9abe82a84b34e0d05d5941bd97f5a3fcad34f17d
PavanKumarBollu <bollupavankumar12@gmail.com>
2025-01-25 17:48:00 +0530

    firtst verison of the project using html and css

---
+++ javascipt/logic.js
@@ -0,0 +1,11 @@
+// document.querySelector("#btn").addEventListener("click",()=>{
+//     console.log(document.getElementById("options-container").value);
+//     console.log("working");
+    
+// });
+
+// document.getElementsByName("options-container").forEach(ele =>{
+//     console.log(ele);
+//     console.log(ele.value)
+    
+// });
